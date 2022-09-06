e869e05f5cb6edfd19cafaf7df0c4459704e00f5
paliwalamol <amolpaliwalct19@acropolis.in>
2022-09-07 00:39:07 +0530

    some final changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [res,setRes] = useState(null);
-  const [but,SetButton]  = useState(true);
-  const [inpt,setInput]  = useState("");
-  const [city,setCity] = useState(null);
-  const [weather,setWeather] = useState(null);
-  const [load,setLoad] = useState(false);
+
+  const [res, setRes] = useState(null);
+  const [but, SetButton] = useState(true);
+  const [inpt, setInput] = useState("");
+  const [city, setCity] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [load, setLoad] = useState(false);
 
 
   const setInputfunc = (e) => {
     setInput(e.target.value);
-    // console.log(e.target.value);
-    if(e.target.value === ""){
+    if (e.target.value === "") {
       SetButton(true);
     }
-    else{
-    SetButton(false);
+    else {
+      SetButton(false);
     }
     setRes(null);
   }
@@ -25,100 +25,93 @@ function App() {
   const fetchData = (e) => {
     setLoad(true);
     e.preventDefault();
-    console.log("fetching data");
     fetch(`https://restcountries.com/v3.1/name/${inpt}?fullText=true`).then((res) => res.json()).then((data) => {
       setRes(data);
-      if(data && data.status!==404){
+      if (data && data.status !== 404) {
         setCity(data[0].capital);
       }
       setLoad(false);
     }
-    );  
-    
+    );
+
   }
 
-  // console.log(res);
-
   const setInitial = () => {
-    console.log("set initial button clicked");
     setRes(null);
     setInput("");
     SetButton(true);
-    setWeather(null);  
-  }
-  
-  const getWeather = () => {
-    setLoad(true);
-    console.log("get weather button clicked");
-    fetch(`http://api.weatherstack.com/current?access_key=17a211a98de9e9420bd480d8d82ec189&query=${city}`).then((res) => res.json()).then((data) => {
-      // console.log(data);
-      setWeather(data);
-      setLoad(false);
-  });
-  
+    setWeather(null);
   }
 
+  const getWeather = () => {
+    setLoad(true);
+    fetch(`http://api.weatherstack.com/current?access_key=17a211a98de9e9420bd480d8d82ec189&query=${city}`).then((res) => res.json()).then((data) => {
+
+      setWeather(data);
+      setLoad(false);
+    });
+
+  }
 
   return (
     <div className="App">
-    <h1>Weather App</h1>
-    
-    {!res && 
-      <>
-      <form className='box'>
-      {load===true && 
-        <div className="loader"></div>   
+      <h1>Weather App</h1>
+
+      {!res &&
+        <>
+          <form className='box'>
+            {load === true &&
+              <div className="loader"></div>
+            }
+            <input placeholder='Enter country Name' className="inp" type="text" value={inpt} onChange={(e) => setInputfunc(e)} />
+            <button className="btn" disabled={but} onClick={(e) => fetchData(e)}>Submit</button>
+          </form>
+        </>
       }
-      <input placeholder='Enter country Name' className="inp" type="text" value={inpt} onChange={(e)=>setInputfunc(e)}/>
-      <button className="btn" disabled={but} onClick={(e)=>fetchData(e)}>Submit</button>
-      </form>
-      </>
-      }
-  
-      {!weather && res && res?.status!==404 && res.map((item,key) => {
-        
+
+      {!weather && res && res?.status !== 404 && res.map((item, key) => {
+
         return (
           <div className="box" key={key}>
-          {load===true && 
-            <div className="loader"></div>   
-          }
+            {load === true &&
+              <div className="loader"></div>
+            }
             <h3>Capital: {item.capital}</h3>
             <h3>Population: {item.population}</h3>
             <h3>Latitude:{item.latlng[0]} longitude:{item.latlng[1]}</h3>
-            
+
             <img src={item.flags.png} name="flag" alt="flagimage" className="image" />
             <button className='btn' onClick={getWeather}>Get Capital Weather</button>
             <button className='btn' onClick={setInitial}>Back</button>
           </div>
         )
-        })}
-        {!weather && res?.status===404 && 
-          <div className='box'>
-          {load===true && 
-            <div className="loader"></div>   
+      })}
+      {!weather && res?.status === 404 &&
+        <div className='box'>
+          {load === true &&
+            <div className="loader"></div>
           }
           <h1>Country not found</h1>
           <button className='btn' onClick={setInitial}>Back</button>
-          </div>
-        
+        </div>
+
       }
-      {weather && 
-           
-          <div className="box">
-          {load===true && 
-            <div className="loader"></div>   
+      {weather &&
+
+        <div className="box">
+          {load === true &&
+            <div className="loader"></div>
           }
-            <h1>Weather in {city}</h1>
-            <img src={weather.current.weather_icons[0]} alt="weatherImage" />
-            <h3>Temperature: {weather.current.temperature}°C </h3>
-            <h3>Humidity: {weather.current.humidity}</h3>
-            <h3>Description: {weather.current.weather_descriptions}</h3>
-            <button className='btn' onClick={setInitial}>Back to Home</button>  
-            </div>
-            
+          <h1>Weather in {city}</h1>
+          <img src={weather.current.weather_icons[0]} alt="weatherImage" />
+          <h3>Temperature: {weather.current.temperature}°C </h3>
+          <h3>Humidity: {weather.current.humidity}</h3>
+          <h3>Description: {weather.current.weather_descriptions}</h3>
+          <button className='btn' onClick={setInitial}>Back to Home</button>
+        </div>
+
       }
-        
-      
+
     </div>
   );
 }
